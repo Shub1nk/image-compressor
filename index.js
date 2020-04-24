ae381d95cf73a5de23e73b983eee55200b2c1a0a
Nikolay Shubin <nashubin@alfabank.ru>
2020-04-24 19:27:03 +0500

    поправить логику

--- index.js
+++ index.js
@@ -16,26 +16,30 @@ const dayUpdate = day < 10 ? `0${day}` : day;
 const monthUpdate = month < 10 ? `0${month}` : month;
 // -----------------------------------------
 
-// TODO: завернуть в рекурсию
-fs.readdir("./images", (err, pages) => {
-  if (err) throw err;
-  pages.filter(page => page !== '.DS_Store').map(page => {
-    fs.readdir(`./images/${page}`, (err, devices) => {
-      if (err) throw err;
-      devices.filter(page => page !== '.DS_Store').map(device => {
-        fs.readdir(`./images/${page}/${device}`, (err, fileNames) => {
-          if (err) throw err;
-          fileNames.filter(fileName => fileName !== '.gitkeep').forEach(file => {
-            const { width, height, type } = sizeOf(`./images/${page}/${device}/${file}`);
-
-            // [страница]_[описание]_[размеры]_[common|desktop|mobile]_[дата].[формат]
-            const source = tinify.fromFile(`./images/${page}/${device}/${file}`);
-            source.toFile(`./result/${page}_${file.split('.')[0]}_${width}x${height}_${device}_${dayUpdate}-${monthUpdate}-${year}.${type}`);
+fs.mkdir('./result', () => {
+  // TODO: завернуть в рекурсию
+  fs.readdir("./images", (err, pages) => {
+    if (err) throw err;
+    pages.filter(page => page !== '.DS_Store' && page !== '.gitkeep').map(page => {
+      fs.readdir(`./images/${page}`, (err, devices) => {
+        if (err) throw err;
+        devices.filter(device => device !== '.DS_Store' && device !== '.gitkeep').map(device => {
+          fs.readdir(`./images/${page}/${device}`, (err, fileNames) => {
+            if (err) throw err;
+            fileNames.filter(fileName => fileName !== '.gitkeep').forEach(file => {
+              const { width, height, type } = sizeOf(`./images/${page}/${device}/${file}`);
+  
+              // [страница]_[описание]_[размеры]_[common|desktop|mobile]_[дата].[формат]
+              const source = tinify.fromFile(`./images/${page}/${device}/${file}`);
+              source.toFile(`./result/${page}_${file.split('.')[0]}_${width}x${height}_${device}_${dayUpdate}-${monthUpdate}-${year}.${type}`);
+            })
           })
         })
       })
     })
-  })
-});
+  });
+
+})
+
 
 
